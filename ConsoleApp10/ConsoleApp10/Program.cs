4680f9ba1c0b7c07b17bc53efc276ce514246913
Vladboulanov <vboulanov@gmail.com>
2019-11-02 19:06:23 +0300

    test

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp10
+{
+    class Array1Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Здравствуйте, " + name);
+            Console.ReadKey();
+            Thread.Sleep(5000);
+            Console.WriteLine("До свидания, " + name);
+            Console.ReadKey();
+        }
+    }
+}

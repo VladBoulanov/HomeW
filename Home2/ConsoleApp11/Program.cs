eee6b2c08c79c4c2cfa7b9cbb7fcd4dc259c8e37
Vladboulanov <vboulanov@gmail.com>
2019-11-04 19:18:16 +0300

    Вторая ДЗ

---
+++ Home2/ConsoleApp11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            int b = int.Parse(Console.ReadLine());
+            char operand = char.Parse(Console.ReadLine());
+            if(operand == '+')
+            {
+                Console.WriteLine("a + b = " + (a + b));
+            }
+            if (operand == '-')
+            {
+                Console.WriteLine("a - b = " + (a - b));
+            }
+            if (operand == '*')
+            {
+                Console.WriteLine("a * b = " + (a * b));
+            }
+            if (operand == '/')
+            {
+                Console.WriteLine("a / b = " + (a / b));
+            }
+            if (operand == '%')
+            {
+                Console.WriteLine("a % b = " + (a % b));
+            }
+            if (operand == '^')
+            {
+                Console.WriteLine("a ^ b = " + (a ^ b));
+            }
+        }
+    }
+}

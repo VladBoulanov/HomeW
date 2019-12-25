c2653cea297ba12b0a8c9a4cd167e0bf28964fcf
Vladboulanov <vboulanov@gmail.com>
2019-12-25 20:29:18 +0300

    Home Work7

---
+++ HW7/HW7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> charList = new List<char> (Console.ReadLine());
+            int cht = 0;
+            int sum = 0;
+            foreach (char number in charList)
+            {
+                if (number=='(')
+                {
+                    cht++;
+                }
+                else if(number == ')')
+                {
+                    cht--;
+                }
+               else if(number == '[')
+               {
+                   sum++;
+               }
+               else if (number == ']')
+               {
+                   sum--;
+               }
+
+            }
+            if (cht == 0 || sum == 0)
+                Console.WriteLine("Правильно");
+            else
+                Console.WriteLine("Неправильно");
+            
+        }
+    }
+}
